4639fe83dfde7a47c7ca062626f3ea41b873ba62
nathanberry97 <nathanberry97@live.com>
2024-01-09 22:10:39 +0000

    started work on game

--- src/main.lua
+++ src/main.lua
@@ -1,3 +1,57 @@
-local function helloWorld() print("Hello World!") end
+import "CoreLibs/graphics"
 
-helloWorld()
+local pd<const> = playdate
+local gfx<const> = playdate.graphics
+
+-- Player variables
+local playerX, playerY = 200, 120
+local playerSpeed = 3
+local velocityX, velocityY = 0, 0
+
+-- Apple variables
+local appleX, appleY = math.random(0, 400), math.random(0, 240)
+
+local function playerMovement()
+    -- Player velocity
+    if pd.buttonIsPressed(pd.kButtonUp) then
+        velocityY, velocityX = -playerSpeed, 0
+    elseif pd.buttonIsPressed(pd.kButtonDown) then
+        velocityY, velocityX = playerSpeed, 0
+    elseif pd.buttonIsPressed(pd.kButtonLeft) then
+        velocityX, velocityY = -playerSpeed, 0
+    elseif pd.buttonIsPressed(pd.kButtonRight) then
+        velocityX, velocityY = playerSpeed, 0
+    end
+
+    -- Update player position
+    playerX = playerX + velocityX
+    playerY = playerY + velocityY
+end
+
+local function playerCollision()
+    -- Player collision
+    if playerX < 0 then
+        playerX = 400
+    elseif playerX > 400 then
+        playerX = 0
+    end
+
+    if playerY < 0 then
+        playerY = 240
+    elseif playerY > 240 then
+        playerY = 0
+    end
+end
+
+function pd.update()
+    -- Update the game state here
+    gfx.clear()
+
+    -- Draw the game state here
+    gfx.fillRect(playerX, playerY, 10, 10)
+    gfx.fillCircleAtPoint(appleX, appleY, 5)
+
+    -- Update player position
+    playerMovement()
+    playerCollision()
+end
